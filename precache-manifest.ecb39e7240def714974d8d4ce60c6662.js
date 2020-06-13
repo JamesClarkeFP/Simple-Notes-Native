28bb9c9e935f0238311e488e5ad7ac16501ce055
Phoenix <JamesClarkeFP@gmail.com>
2020-06-13 09:29:20 +0100

    Updates

---
+++ precache-manifest.ecb39e7240def714974d8d4ce60c6662.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "1681c380d534fbe3ec4b",
+    "url": "/Native-Simple-Notes/static/js/app.86628808.chunk.js"
+  },
+  {
+    "revision": "f86bba3a49d898ad7b76",
+    "url": "/Native-Simple-Notes/static/js/runtime~app.a5cd0704.js"
+  },
+  {
+    "revision": "ff9a888920dcec16a2bf",
+    "url": "/Native-Simple-Notes/static/js/2.ff2a15a8.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Native-Simple-Notes/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/Native-Simple-Notes/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/Native-Simple-Notes/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/Native-Simple-Notes/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/Native-Simple-Notes/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Native-Simple-Notes/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/Native-Simple-Notes/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/Native-Simple-Notes/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/Native-Simple-Notes/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/Native-Simple-Notes/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Native-Simple-Notes/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/Native-Simple-Notes/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/Native-Simple-Notes/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/Native-Simple-Notes/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/Native-Simple-Notes/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/Native-Simple-Notes/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/Native-Simple-Notes/favicon.ico"
+  },
+  {
+    "revision": "ec981420c65b247bfe8979a3a5cedd2b",
+    "url": "/Native-Simple-Notes/index.html"
+  },
+  {
+    "revision": "b6c2b26378b5516b7368debb813b5ea6",
+    "url": "/Native-Simple-Notes/manifest.json"
+  },
+  {
+    "revision": "518c1a5c3f70e542b0a6c558145b14f9",
+    "url": "/Native-Simple-Notes/register-service-worker.js"
+  }
+];
